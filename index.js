81f3aca7b90642459b013569b325b3bd04938911
jinook929 <jinook929@gmail.com>
2021-04-23 18:20:03 -0400

    commit tree 51a50873c331876cafb00ce1e69d4cce3d984e9b

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(phrase) {
+    return phrase[0].toUpperCase() + phrase.slice(1)
+  }
+  
+  static sanitize(phrase) {
+    return phrase.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  
+  static titleize(phrase) {
+    return phrase.split(" ").map((w, i) => {
+      const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      if(!exceptions.includes(w) || i === 0) {return Formatter.capitalize(w)}
+      else {return w}
+    }).join(" ")
+  }
 }
+
+console.log( Formatter.titleize( "getting giggles" ) )
+console.log( Formatter.titleize( "where the wild things are" ) )
+console.log( Formatter.titleize( "chicken soup with rice and a few other songs" ) )
+console.log( Formatter.titleize( "Maurice a an but of and for at by from end" ) )
+console.log( Formatter.titleize( "a tale of two cities" ) )
+console.log( Formatter.titleize( "in the night kitchen" ) )
